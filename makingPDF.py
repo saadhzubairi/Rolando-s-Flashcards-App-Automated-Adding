cd9e9af01a42396e1c8e0f47f10e1843d171615c
Saad Zubairi <saadhzubairi@outlook.com>
2023-08-25 02:47:21 +0500

    pdf adder

--- makingPDF.py
+++ makingPDF.py
@@ -13,26 +13,37 @@ italic_style = ParagraphStyle("ItalicStyle", parent=getSampleStyleSheet()["Norma
 italic_style.textColor = colors.black
 italic_style.fontName = "Italic"
 
+
 # Read data from CSV and generate PDF
 def generate_pdf(csv_filename, pdf_filename):
     data = []
+
+    style = ParagraphStyle(name="Normal", fontSize=25, alignment=1, textColor="#000")
+
+    blueStyle = ParagraphStyle(
+        name="Normal", fontSize=16, alignment=0, textColor="#1177aa"
+    )
+
+    paragraph = Paragraph(f"GRE Words List<br/><br/><br/><br/>", style=style)
+
     with open(csv_filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         ins = 0
         for row in csv_reader:
-            ins+=1
+            ins += 1
             if len(row) == 3:
                 word, definition, _ = row
-                data.append(
-                    f'<b>{word}</b> : <i>{definition}</i><br/><br/>'
-                )
+                data.append(Paragraph(f"<b>{ins}. {word}</b>:<br/><br/>", style=blueStyle))
+                data.append(Paragraph(f"<i>{definition}</i><br/><br/>"))
 
     doc = SimpleDocTemplate(pdf_filename, pagesize=letter)
-    flowables = [Paragraph(para) for para in data]
+    flowables = [paragraph]
+    flowables += data
+
     doc.build(flowables)
 
 
 if __name__ == "__main__":
-    csv_filename = "output.csv"  # Replace with your CSV file's name
-    pdf_filename = "words.pdf"  # Replace with your desired output PDF file's name
+    csv_filename = "output.csv"
+    pdf_filename = "words.pdf"
     generate_pdf(csv_filename, pdf_filename)
